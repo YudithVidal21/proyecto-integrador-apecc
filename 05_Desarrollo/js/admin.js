0cb602380ed103a9771be787ce5dbd58a6bb54be
YudithVidal21 <YudithVidal21@users.noreply.github.com>
2026-09-21 00:47:34 -0500

    Fix login url to localhost directly

--- 05_Desarrollo/js/admin.js
+++ 05_Desarrollo/js/admin.js
@@ -72,7 +72,7 @@ async function iniciarSesion(event) {
     errorMsg.style.display = 'none';
 
     try {
-        const res = await fetch(`${API_URL}/auth/login`, {
+        const res = await fetch(`http://localhost:3000/auth/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
